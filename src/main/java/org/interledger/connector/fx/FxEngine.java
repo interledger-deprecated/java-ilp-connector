61811e05db269b03fa3235e1b663acb8212666d2
sappenin <sappenin@gmail.com>
2017-11-28 11:10:11 -0700

    Remove unused method.

--- src/main/java/org/interledger/connector/fx/FxEngine.java
+++ src/main/java/org/interledger/connector/fx/FxEngine.java
@@ -1,11 +1,7 @@
 package org.interledger.connector.fx;
 
-import org.interledger.InterledgerAddress;
 import org.interledger.connector.routing.Route;
-import org.interledger.plugin.lpi.Transfer;
-
-import java.math.BigInteger;
-import java.util.Objects;
+import org.interledger.connector.routing.RoutingTable;
 
 import javax.money.convert.ExchangeRateProvider;
 
@@ -16,35 +12,9 @@ import javax.money.convert.ExchangeRateProvider;
  * routing table. This is because it is expected that either ILQP will go away (e.g., ILP3) and thus the routing table
  * will have no concept of a liquidity curve; OR  that FX calculations will be made in real-time during quoting (e.g.,
  * ILP Atomic-mode). However, implementations that wish to implement ILQP would be free to do so, and can still utilize
- * this interface, connected to an extension of {@link org.interledger.connector.routing.RoutingTable} with an extended
- * {@link Route} to accomplish ILQP + ILP1 type behavior.
+ * this interface, connected to an extension of {@link RoutingTable} with an extended {@link Route} to accomplish ILQP +
+ * ILPv1 type behavior.
  */
 public interface FxEngine extends ExchangeRateProvider {
-
-  /**
-   * Given a source transfer, compute the amount (in local units of the destination ledger) that should be transferred
-   * to the next-hop local ledger.
-   */
-  default BigInteger computeNextHopLocalTransferAmount(
-      final Transfer sourceTransfer,
-      final InterledgerAddress destinationLedgerPrefix
-  ) {
-    Objects.requireNonNull(sourceTransfer);
-    InterledgerAddress.requireAddressPrefix(destinationLedgerPrefix);
-
-
-
-
-
-
-
-
-
-
-
-
-
-    return null;
-  }
 
 }
